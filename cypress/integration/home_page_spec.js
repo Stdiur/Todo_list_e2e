dc7999e891ab2ec1eeea98100ab23c5ac56f2f24
Stdiur <wolove8023@vip.qq.com>
2019-06-08 22:56:47 +0800

    Add files via upload

---
+++ cypress/integration/home_page_spec.js
@@ -0,0 +1,28 @@
+﻿describe('Todo App Test', function() {
+  it('successfully loads', function() {
+    cy.visit('/');
+    cy.get('input').type('待办A');
+    cy.contains('Add').click();
+
+    cy.get('input').type('待办B');
+    cy.contains('Add').click();
+
+    cy.get('input').type('待办C');
+    cy.contains('Add').click();
+
+    cy.pause(1000);
+
+
+    cy.get('.delete-btn').first().click();
+
+    cy.contains('Edit').first().click();
+    cy.get('#app > div > table > tbody > tr:nth-child(1) > td:nth-child(1) > form > input').clear()
+        .type("修改待办A");
+    cy.get('button').contains('Save').click();
+
+    cy.pause(1000);
+
+    cy.contains('修改待办A').click();
+
+  })
+})
